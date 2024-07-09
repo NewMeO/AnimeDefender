6d8890a1ecad49cc263c854f90ec5d26a661a9ec
NewMeO <90089711+NewMeO@users.noreply.github.com>
2024-07-09 22:34:13 +0700

    Create Maid.lua

---
+++ Maid.lua
@@ -0,0 +1,27 @@
+local Maid = {}
+Maid.ClassName = "Maid"
+
+--- Returns a new Maid object
+-- @constructor Maid.new()
+-- @treturn Maid
+function Maid.new()
+	return setmetatable({
+		_tasks = {}
+	}, Maid)
+end
+
+--- Same as indexing, but uses an incremented number as a key.
+-- @param task An item to clean
+-- @treturn number taskId
+function Maid:GiveTask(task)
+	if not task then
+		error("Task cannot be false or nil", 2)
+	end
+
+	local taskId = #self._tasks+1
+	self[taskId] = task
+
+	return taskId
+end
+
+return Maid;
